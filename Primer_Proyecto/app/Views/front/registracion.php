a5c7cfbf37c458fd84ef5fbcc3e542601f3ed47c
Juan Ignacio <juanigmw@gmail.com>
2024-07-09 22:47:56 -0300

    Se agrego la carpeta RProT2_37062451 con el proyecto Cafeteria Nippon al repositorio

---
+++ Primer_Proyecto/app/Views/front/registracion.php
@@ -0,0 +1,91 @@
+<!-- 
+ **************************************************************************************************** 
+    
+    Autor: Meza Wakahayashi, Juan Ignacio
+    Curso: Talentos Digitales
+    Modulo: II
+    Trabajo Parctico: Tarea Integradora 2. 
+
+    Este archivo PHP trata sobre la pagina donde se indica "la creacion de cuenta para clientes de la empresa (registro)"
+
+*****************************************************************************************************
+    -->
+
+<div class="container" >
+    <div class="row justify-content-center align-items-center" style="height: 90vh;">
+        <div class="col-12 col-md-8 col-lg-6">
+            <div class="card p-1">
+  
+               <section>
+                 <p>
+                  <h2 class="h3">Registro para Nuestros Clientes</h2>
+                 </p>
+               </section>
+             <form class="input-group mb-3">
+              
+              <div class="input-group mb-3">
+                 
+                    <input type="text" class="form-control text-uppercase" placeholder="Nombres" aria-label="nombres" aria-describedby="basic-addon1" required>
+                
+              </div>
+              
+              <div class="input-group mb-3">
+  
+                <input type="text"  class="form-control text-uppercase" placeholder="Apellidos" aria-label="apellidos" aria-describedby="basic-addon1" required>
+              </div>
+
+
+              <div class="input-group mb-3">
+                
+                <input type="email" class="form-control" 
+                  placeholder="correo" aria-label="Recipient's username" aria-describedby="basic-addon2" required>
+              </div>
+
+               <div class="input-group mb-3">
+  
+                 <input type="text" class="form-control"  placeholder="DNI" aria-label="dni" aria-describedby="basic-addon1" maxlength ="8"required>
+               </div>
+
+               <div class="input-group mb-3">
+  
+                       <input type="text" class="form-control" placeholder="Domicilio" aria-label="domicilio" aria-describedby="basic-addon1" required>
+                </div>
+
+                <div class="input-group mb-3">
+  
+                     <input type="text" class="form-control" placeholder="Nombre Usuario" aria-label="tel" aria-describedby="basic-addon1" required>
+                </div>
+
+
+                <div class="input-group mb-3">
+  
+                     <input type="password" class="form-control" placeholder="Contraseña" aria-label="tel" aria-describedby="basic-addon1" minlength="6" maxlength="8" required>
+                </div>
+                 
+                
+                 <section>
+                         <p>
+                            <h6 class="h6">Para finalizar haga clic en el boton Registrarse</h6>
+                            
+                         </p>
+                </section>
+         </div><!-- aca termina el 4to div -->
+
+                <div  class="d-flex justify-content-center">
+            
+                     <button type="submit" class="btn btn-outline-success mx-5">Registrarse</button>
+
+                      <button type="reset" class="btn btn-outline-danger">Cancelar</button>              
+                  </div>
+<!-- aca va el cierre de form-->
+</form>
+         </div>
+      </div>
+     
+</div>
+ <section>
+          <p>
+              
+          </p>
+ </section>
+        
